e660df5818bf3812f4423ec0914a4f5fcb1eeb56
ningli <ningli@vt.edu>
2016-05-23 15:38:23 -0400

    Wolco1

--- fenton_fungi/spiders/fenton_fungi_spider.py
+++ fenton_fungi/spiders/fenton_fungi_spider.py
@@ -2,16 +2,18 @@ import scrapy
 
 class FentonFungiSpider(scrapy.Spider):
     name = "fenton_fungi_spider"
-    baseurl = 'http://genome.jgi.doe.gov/cgi-bin/getDbSeq?db=Pospl1&searchTabList=protein,proteinHitDesc&hitSeqList='
+    # baseurl = 'http://genome.jgi.doe.gov/cgi-bin/getDbSeq?db=Pospl1&searchTabList=protein,proteinHitDesc&hitSeqList='
+    baseurl = 'http://genome.jgi.doe.gov/cgi-bin/getDbSeq?db=Wolco1&searchTabList=protein,proteinHitDesc&hitSeqList='
     # build url with interested ids
     start_urls = []
     # read data
-    f = open("data", "r")
+    f = open("GSE78007-2FC", "r")
     for line in f:
-        start_urls.append(baseurl + line.split('_')[0])
+        start_urls.append(baseurl + line)
+        # start_urls.append(baseurl + line.split('_')[0])
     f.close()
 
     def parse(self, response):
         if response.status == 200: # search found
-            with open('results.txt', 'a') as f:
+            with open('results2.txt', 'a') as f:
                 f.write(response.xpath('//div[@class="home"]/pre/text()').extract()[0])
